1e8f7950b6b6477cbda500d14bcf6fd8a5c1dbfc
st4rgaz3r1010 <bintanggibran10@gmail.com>
2021-10-27 20:40:20 +0700

    Development and Coding by Muhammad Rifqi Nugraha - Kelompok Coding - Capstone Project Progress #2

--- app/src/main/java/com/example/hellogit/NavActivity.java
+++ app/src/main/java/com/example/hellogit/NavActivity.java
@@ -6,10 +6,6 @@ import android.view.MenuItem;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import com.example.hellogit.ui.account.AccountFragment;
 import com.example.hellogit.ui.cart.CartFragment;
@@ -24,19 +20,16 @@ public class NavActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nav);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
         BottomNavigationView bottomNav = findViewById(R.id.nav_view);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                 new HomeFragment()).commit();
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_collection,
-                R.id.navigation_cart, R.id.navigation_account).build();
-        NavController navController = Navigation.findNavController(this, R.id.fragment_container);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(bottomNav, navController);
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener = new
